7ab7e45430acc475ca4cb7fdddd7c20d70170adc
Andrey Kartashov <gorilych@gmail.com>
2021-08-20 00:10:41 +0700

    rust: reverse-string

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,20 @@
+#[cfg(not(feature = "grapheme"))]
 pub fn reverse(input: &str) -> String {
-    unimplemented!("Write a function to reverse {}", input);
+    let mut res = String::with_capacity(input.len());
+    for c in input.to_string().chars().rev() {
+        res.push(c);
+    }
+    res
+}
+
+#[cfg(feature = "grapheme")]
+use unicode_segmentation::UnicodeSegmentation;
+
+#[cfg(feature = "grapheme")]
+pub fn reverse(input: &str) -> String {
+    let mut res = String::with_capacity(input.len());
+    for c in input.to_string().graphemes(true).rev() {
+        res.push_str(c);
+    }
+    res
 }
