b1f4c14f86312a676b09a21f0249d32dadd6458f
Andrey Kartashov <gorilych@gmail.com>
2021-08-20 23:52:20 +0700

    rust: sum-of-multiples

---
+++ rust/sum-of-multiples/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    (1..limit)
+    .filter(|x| factors
+        .iter()
+        .filter(|&&f| f != 0)
+        .any(|&f| (f != 0) && (x % f == 0)))
+    .sum()
+}
