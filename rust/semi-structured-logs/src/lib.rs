b327e70bc73f9b1a58d52e09b2b3b81ef42993ef
Andrey Kartashov <gorilych@gmail.com>
2021-09-02 18:00:09 +0700

    rust: semi-structured-logs

---
+++ rust/semi-structured-logs/src/lib.rs
@@ -0,0 +1,44 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+use std::fmt;
+
+/// various log levels
+#[derive(Clone, PartialEq, Debug)]
+pub enum LogLevel {
+    Info,
+    Warning,
+    Error,
+}
+
+impl fmt::Display for LogLevel {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match &self {
+                LogLevel::Info => "INFO",
+                LogLevel::Error => "ERROR",
+                LogLevel::Warning => "WARNING",
+            },
+        )
+    }
+}
+
+/// primary function for emitting logs
+pub fn log(level: LogLevel, message: &str) -> String {
+    std::format!(
+        "[{}]: {}",
+        level,
+        message,
+    )
+}
+pub fn info(message: &str) -> String {
+    log(LogLevel::Info, message)
+}
+pub fn warn(message: &str) -> String {
+    log(LogLevel::Warning, message)
+}
+pub fn error(message: &str) -> String {
+    log(LogLevel::Error, message)
+}
