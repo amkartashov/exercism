7dd495fbcd1324bc3a3733c4091b02c1c12688b3
Andrey Kartashov <gorilych@gmail.com>
2021-09-05 03:15:15 +0700

    rust: matching-brackets: use macro

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,29 +1,37 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    // array of bracket pairs: [ (opening, closing) ...]
-    const BRACKETS: [(char, char); 3] = [('[', ']'), ('{', '}'), ('(', ')')];
-
     // vector of expected closing brackets
     let mut expected_close_brackets = Vec::new();
 
-    for c in string.chars() {
-        for (open, close) in BRACKETS {
-            match c {
-                o if o == open => expected_close_brackets.push(close),
-                c if c == close => {
-                    // check that this is what we expect
-                    if let Some(expected) = expected_close_brackets.pop() {
-                        if expected != close {
-                            // wrong closing bracket
+    macro_rules! brackets_stack_check {
+        ($stack:ident, $c:ident, $($open:literal $close:literal),+ $(,)?) => {
+            match $c {
+                $(
+                    $open => $stack.push($close),
+                    $close => {
+                        if let Some(expected) = $stack.pop() {
+                            if expected != $close {
+                                // wrong closing bracket
+                                return false;
+                            }
+                        } else {
+                            // we don't expect closing bracket here
                             return false;
-                        }
-                    } else {
-                        // we don't expect closing bracket here
-                        return false;
-                    };
-                }
-                _ => {}
-            };
-        }
+                        };
+                    },
+                )*
+                _ => {},
+            }
+        };
+    }
+
+    for c in string.chars() {
+        brackets_stack_check!(
+            expected_close_brackets,
+            c,
+            '{' '}',
+            '(' ')',
+            '[' ']',
+        );
     }
 
     expected_close_brackets.is_empty()
