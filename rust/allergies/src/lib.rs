528cac2dbf22746f87c4323de40b64f0f8279623
Andrey Kartashov <gorilych@gmail.com>
2021-09-07 21:16:49 +0700

    rust: allergies: use enum discriminant

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -2,14 +2,14 @@ pub struct Allergies(u32);
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Allergen {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats,
+    Eggs = 1 << 0,
+    Peanuts = 1 << 1,
+    Shellfish = 1 << 2,
+    Strawberries = 1 << 3,
+    Tomatoes = 1 << 4,
+    Chocolate = 1 << 5,
+    Pollen = 1 << 6,
+    Cats = 1 << 7,
 }
 
 const ALL_ALLERGENS: [Allergen; 8] = [
@@ -25,16 +25,7 @@ const ALL_ALLERGENS: [Allergen; 8] = [
 
 impl Allergen {
     pub fn score(&self) -> u32 {
-        match *self {
-            Allergen::Eggs => 1 << 0,
-            Allergen::Peanuts => 1 << 1,
-            Allergen::Shellfish => 1 << 2,
-            Allergen::Strawberries => 1 << 3,
-            Allergen::Tomatoes => 1 << 4,
-            Allergen::Chocolate => 1 << 5,
-            Allergen::Pollen => 1 << 6,
-            Allergen::Cats => 1 << 7,
-        }
+        *self as u32
     }
 }
 
