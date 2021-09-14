2c0390158a468427a70b52814d03a08874dc5ca4
Andrey Kartashov <gorilych@gmail.com>
2021-09-14 21:23:05 +0700

    rust: parallel-letter-frequency

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,12 +1,41 @@
+use crossbeam;
 use std::collections::HashMap;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    unimplemented!(
-        "Count the frequency of letters in the given input '{:?}'. Ensure that you are using {} to process the input.",
-        input,
-        match worker_count {
-            1 => "1 worker".to_string(),
-            _ => format!("{} workers", worker_count),
-        }
-    );
+    fn frequency_worker(input: &[&str]) -> HashMap<char, usize> {
+        input
+            .iter()
+            .flat_map(|s| s.chars())
+            .filter(|c| c.is_alphabetic())
+            .flat_map(char::to_lowercase)
+            .fold(HashMap::new(), |mut m, c| {
+                let entry = m.entry(c).or_default();
+                *entry += 1;
+                m
+            })
+    }
+
+    let mut chunk_size = input.len() / worker_count;
+    if chunk_size == 0 {
+        chunk_size = 1;
+    }
+
+    crossbeam::scope(|s| {
+        let threads: Vec<_> = input
+            .chunks(chunk_size)
+            .map(|input| s.spawn(move |_| frequency_worker(input)))
+            .collect();
+
+        threads
+            .into_iter()
+            .fold(HashMap::new(), |mut totals, thread| {
+                let m = thread.join().unwrap();
+                for (c, f) in m.iter() {
+                    let entry = totals.entry(*c).or_default();
+                    *entry += f;
+                }
+                totals
+            })
+    })
+    .unwrap()
 }
